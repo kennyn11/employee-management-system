12c9c7e2265623559d4c1f1d0a4ddaf64ce27644
Kenny Nguyen <kennyn@usf.edu>
2026-03-01 18:15:16 -0500

    added managers and employees

--- cc_6.js
+++ cc_6.js
@@ -11,3 +11,21 @@ class Employee {
 }
 
 // Manager sub class //
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+describe() {
+    return `${this.name} is a manager for this ${this.department} department including
+    ${this.teamSize}`;
+}
+}
+
+// Managers and employees //
+const emp1 = new Employee ("John", "Grocery",);
+const emp2 = new Employee ("Tom", "Produce",)
+const emp3 = new Employee ("Sophia", "Seafood",)
+const man1 = new Manager ("Morgan", "Deli", 25)
+const man2 = new Manager ("James", "Baker", 15)
+const man3 = new Manager ("Pubert", "Customer Service", 50)
